23294fb08596048a0b19aaca71f98ed4353850b0
KeziMewing <kezispecialisthub@gmail.com>
2024-11-12 20:12:44 +0700

    Update Testing.lua

--- Testing.lua
+++ Testing.lua
@@ -1,45 +1,88 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local CurrentVersion = "Kezi Hub V1"
 
-local Window = Rayfield:CreateWindow({
-   Name = "🚗 CDID Script 🚗",
-   LoadingTitle = "Dreamy Hub",
-   LoadingSubtitle = "by antdonut23",
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "examplehub"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
+    -- Call Library
+    local GUI = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local MainTab = Window:CreateTab("🏠 Home", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Main")
+    -- Main Frame
+    local Window = GUI:CreateWindow({
+        Name = "CurrentVersion",
+        LoadingTitle = "KeziHub Suite",
+        LoadingSubtitle = "by KezHub Team",
+        Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+     
+        DisableRayfieldPrompts = false,
+     
+        ConfigurationSaving = {
+           Enabled = true,
+           FolderName = nil, -- Create a custom folder for your hub/game
+           FileName = "KeziHub"
+        },
+     
+        Discord = {
+           Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+           Invite = "https://discord.gg/9CnjzkGeVp", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+           RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+        },
+     })
 
-Rayfield:Notify({
-   Title = "You executed the script!",
-   Content = "use this script carefully",
-   Duration = 5,
-   Image = nil,
-   Actions = { -- Notification Buttons
-      Ignore = {
-         Name = "Okay!",
-         Callback = function()
-         print("The user tapped Okay!")
-      end
-   },
-},
-})
+
+    -- Local Ver
+    local TruckAutoFarms = false
+
+    -- AutoFarm Creation
+    local Tab = Window:MakeTab({
+        Name = "Tab 1",
+        Icon = "rbxassetid://136244553696277",
+        TestersOnly = false
+    })
+    
+    --[[
+    Name = <string> - The name of the tab.
+    Icon = <string> - The icon of the tab.
+    TestersOnly = <bool> - Makes the tab not accessible to players.
+    ]]
+
+    local Section = Tab:AddSection({
+        Name = "AutoFarm"
+    })
+    
+    --[[
+    Name = <string> - The name of the section.
+    ]]
+
+
+    Tab:AddButton({
+        Name = "Truck AutoFarm",
+        Callback = function()
+                  print("button pressed")
+                  while TruckAutoFarms do
+                    local args = {
+                        [1] = "Truck"
+                    }
+                    
+                    game:GetService("ReplicatedStorage"):WaitForChild("NetworkContainer"):WaitForChild("RemoteEvents"):WaitForChild("Job"):FireServer(unpack(args))
+                    
+                    wait()
+
+                    local args = {
+                        [1] = "Cirebon"
+                    }
+                    
+                    game:GetService("ReplicatedStorage"):WaitForChild("NetworkContainer"):WaitForChild("RemoteEvents"):WaitForChild("TransEvent"):FireServer(unpack(args))
+
+          end    
+    })
+    
+    --[[
+    Name = <string> - The name of the button.
+    Callback = <function> - The function of the button.
+    ]]
+
+
+
+
+
+
+
+
+end
